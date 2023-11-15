501277dcb9d5a371dce6e0d250d5adade397d618
MasonW419 <mwilkins419@gmail.com>
2023-11-14 22:44:44 -0700

    Pushed Learning Activity 2 Lessons

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -6,3 +6,17 @@ const options = {
     year: "numeric"
 };
 today1.innerHTML = new Date().toLocaleDateString("en-UK", options);
+
+const lastMod = document.querySelector('#last_mod');
+lastMod.innerHTML = new Date(document.lastModified);
+
+
+// Store the selected elements that we are going to use. 
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
