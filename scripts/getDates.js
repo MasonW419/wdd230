cca2fb5175389bbdb67edd79d74e80bf7f9c9544
MasonW419 <mwilkins419@gmail.com>
2023-11-18 13:14:10 -0700

    added hero image to home page

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -20,3 +20,15 @@ hambutton.addEventListener('click', () => {
 	mainnav.classList.toggle('show');
 	hambutton.classList.toggle('show');
 });
+
+
+// page visits
+const visitsDisplay = document.querySelector(".visits");
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+numVisits++;
+localStorage.setItem("numVisits-ls", numVisits);
